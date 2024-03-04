23b1478a0eb0ad153523fb5db2245c958a66d3c8
Zheka <grushevskiy.yevgeniy@mail.ru>
2024-03-04 17:21:38 +0300

    feat: add mutation in schemas

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,6 +1,7 @@
 import { Type } from '@fastify/type-provider-typebox';
 import { GraphQLSchema } from 'graphql';
 import { query } from './queries/query.js';
+import { mutation } from './mutation/mutation.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -21,4 +22,4 @@ export const createGqlResponseSchema = {
   ),
 };
 
-export const  gqlSchema = new GraphQLSchema({ query });
+export const  gqlSchema = new GraphQLSchema({ query, mutation });
